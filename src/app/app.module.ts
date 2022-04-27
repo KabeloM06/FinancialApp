fec3174381f4ca6938c244d18ba807ebeb8cf7cb
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-04-27 20:30:16 +0200

    working on crypto table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoinsTableComponent } from './components/coins-table/coins-table.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoinsTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/coins-table/coins-table.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+interface Coin {
+  id: string
+  image: string
+  name: string
+  symbol: string
+  current_price: number
+  price_change: number
+  total_volume: number
+}
+
+@Component({
+  selector: 'app-coins-table',
+  templateUrl: './coins-table.component.html',
+  styleUrls: ['./coins-table.component.css']
+})
+export class CoinsTableComponent implements OnInit {
+
+  coins: Coin[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    
+    this.http.get<Coin[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .subscribe((res) => {
+      console.log(res);
+    },
+    (error) => console.log(error))
+  };
+
+}
